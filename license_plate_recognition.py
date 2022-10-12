0f2314643a0a7f8ef463572d2b602ae28dd2c59d
Antonio <80766419+AntonioSivaha@users.noreply.github.com>
2022-10-12 15:08:37 +0300

    Tessarect license plate recognition.

---
+++ license_plate_recognition.py
@@ -0,0 +1,61 @@
+import cv2
+import imutils
+import numpy as np
+import pytesseract
+
+
+def license_plate_recognition(image_name=None):
+    """
+    Detect license plate and convert license plate number to image.
+    Return tuple with image name and responsed number or None.
+    """
+
+    img = cv2.imread(image_name, cv2.IMREAD_COLOR)
+    img = cv2.resize(img, (600, 400))
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bilateralFilter(gray, 13, 15, 15)
+
+    edged = cv2.Canny(gray, 30, 200)
+    contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+    screen_cnt = None
+
+    for contour in contours:
+        peri = cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, 0.018 * peri, True)
+
+        if len(approx) == 4:
+            screen_cnt = approx
+            break
+
+    detected = False if screen_cnt is None else True
+
+    if detected:
+        cv2.drawContours(img, [screen_cnt], -1, (255, 0, 0), 4)
+
+    mask = np.zeros(gray.shape, np.uint8)
+    try:
+        new_image = cv2.drawContours(mask, [screen_cnt], 0, 255, -1)
+    except Exception:
+        return (image_name, None)
+
+    new_image = cv2.bitwise_and(img, img, mask=mask)
+
+    (x, y) = np.where(mask == 255)
+    (topx, topy) = (np.min(x), np.min(y))
+    (bottomx, bottomy) = (np.max(x), np.max(y))
+    Cropped = gray[topx:bottomx+1, topy:bottomy+1]
+
+    license_plate = pytesseract.image_to_string(Cropped, config='--psm 11')
+
+    # This block illustrate image and cropped image
+    # img = cv2.resize(img,(500,300))
+    # Cropped = cv2.resize(Cropped,(400,200))
+    # cv2.imshow('car',img)
+    # cv2.imshow('Cropped',Cropped)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    return (image_name, license_plate)
--- main.py
+++ main.py
@@ -1,55 +1,20 @@
-import cv2
-import imutils
-import numpy as np
-import pytesseract
-# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+from license_plate_recognition import license_plate_recognition
+import json
+import os
 
-img = cv2.imread('photos/blurred.jpg', cv2.IMREAD_COLOR)
-img = cv2.resize(img, (600,400) )
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-gray = cv2.bilateralFilter(gray, 13, 15, 15) 
-
-edged = cv2.Canny(gray, 30, 200) 
-contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-contours = imutils.grab_contours(contours)
-contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
-screenCnt = None
-
-for c in contours:
+def main():
+    license_plates = {}
+    for dirname, _, filenames in os.walk('images_jpg/'):
+        for filename in filenames:
+            license_plate_result = license_plate_recognition(dirname + filename)
+            license_plates = {license_plate_result[0]: license_plate_result[1]}
+            print(f'{license_plate_result[0]}: {license_plate_result[1]}]')
     
-    peri = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.018 * peri, True)
- 
-    if len(approx) == 4:
-        screenCnt = approx
-        break
+    print(license_plates)
+    with open('responsed_license_plates.json', 'w') as save_file:
+        json.dump(license_plates, save_file, indent=6)
 
-if screenCnt is None:
-    detected = 0
-    print ("No contour detected")
-else:
-     detected = 1
 
-if detected == 1:
-    cv2.drawContours(img, [screenCnt], -1, (0, 0, 255), 3)
-
-mask = np.zeros(gray.shape,np.uint8)
-new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
-new_image = cv2.bitwise_and(img,img,mask=mask)
-
-(x, y) = np.where(mask == 255)
-(topx, topy) = (np.min(x), np.min(y))
-(bottomx, bottomy) = (np.max(x), np.max(y))
-Cropped = gray[topx:bottomx+1, topy:bottomy+1]
-
-text = pytesseract.image_to_string(Cropped, config='--psm 11')
-print("programming_fever's License Plate Recognition\n")
-print("Detected license plate Number is:",text)
-img = cv2.resize(img,(500,300))
-Cropped = cv2.resize(Cropped,(400,200))
-cv2.imshow('car',img)
-cv2.imshow('Cropped',Cropped)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    main()
